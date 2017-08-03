3e83feb23edef1356901e11ae0fa421adc747936
winkey4986 <winkey4986@sina.com>
2017-08-03 09:59:38 +0800

    [ADD]新增hello2Controller

---
+++ src/main/java/com/wrz/springboottest/hello/Hello2Controller.java
@@ -0,0 +1,16 @@
+package com.wrz.springboottest.hello;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello2")
+public class Hello2Controller {
+    @RequestMapping("get/{id}")
+    public String get(@PathVariable String id){
+        //需要使用add吗
+        System.out.println("收到:"+id);
+        return "hello spring boot:"+id;
+    }
+}
